e282ea41b27bc5d90a87e35ff98c74982ec7ffca
Dev 2 <dev2@gmail.com>
2026-06-26 18:34:58 +0530

    new

--- GitDemoProj1/src/com/nt/test/Test.java
+++ GitDemoProj1/src/com/nt/test/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		Utility u = new Utility();
 		System.out.println(u.getSysDate());
-		System.out.println("Test class");
+		System.out.println("Test class"); 
 		System.out.println("Test class ----abcd");
 
 	}
